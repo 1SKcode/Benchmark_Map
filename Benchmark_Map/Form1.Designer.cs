be2328e36c81544db4cd6a6b0e1910631aa5d029
Александр <ip387525@gmail.com>
2022-11-22 06:01:34 +0300

    Добавьте файлы проекта.

---
+++ Benchmark_Map/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace Benchmark_Map
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.i = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.dataGridView1.Location = new System.Drawing.Point(18, 33);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(556, 122);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.i});
+            this.dataGridView2.Location = new System.Drawing.Point(18, 198);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(556, 125);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Variable Display", 14.25F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(231, 335);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Начать тесты";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(599, 59);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(286, 322);
+            this.panel1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Variable Display", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(3, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(251, 200);
+            this.label3.TabIndex = 0;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Variable Display", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(10, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Процесс выполнения (LOG)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.textBox1.Location = new System.Drawing.Point(18, 348);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 33);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Variable Display", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(34, 327);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Имя тестируемого ПК:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(599, 34);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(286, 26);
+            this.panel2.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Variable Display", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(13, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(361, 26);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Сравнение (коэффициентная форма):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Variable Display", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(13, 6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(238, 26);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Benchmark-карта тестов:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Variable Display", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label6.Location = new System.Drawing.Point(247, 355);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(315, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Выполнение...  (может занять до минуты)";
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Имя ПК";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "1 TEST";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 120;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "2 TEST";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 120;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "3 TEST";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 120;
+            // 
+            // i
+            // 
+            this.i.HeaderText = "I(ср. геом.)";
+            this.i.Name = "i";
+            this.i.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column5.HeaderText = "Имя ПК";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "1 TEST";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 125;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "2 TEST";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 125;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "3 TEST";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            this.Column8.Width = 125;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "I(ср. геом.)";
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.IndianRed;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Variable Display", 8.25F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(472, 335);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 46);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Удалить\r\nзаписи";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 395);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label6);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Benchmark";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn i;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Benchmark_Map/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+
+
+namespace Benchmark_Map
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region ТЕСТЫ
+        public double Test1() // Метод выполнения теста 1 (сортировка массива). Возвращается время выполенения метода В СЕКУНДАХ+миллисекунды
+        {
+            int[] array = new int[45000];
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)  // Инициализация массива рандомными значениями 
+            {
+                array[i] = random.Next(20001); // Присваиваем i элементу рандомное значение от 0 до 20000
+            }
+
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start(); // Начинаем отсчет затраченного времени на интервале (остановка на 50 строке)
+            for (int i = 0; i < array.Length; i++) // Начало выполнения сортировки пузырьком
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            stopWatch.Stop(); // Останавливаем отсчет затраченного времени на интервале (запуск был на 37 строке)
+
+            return stopWatch.ElapsedMilliseconds / 1000f; // Возвращаем значение затраченных секунд на выполнение теста 1
+        }
+
+        public double Test2() // Метод выполнения теста 2 (графические отрисовки). Возвращается время выполенения метода В СЕКУНДАХ+миллисекунды
+        {
+            Graphics g;
+            g = panel1.CreateGraphics();
+            float x = 0f;
+
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start(); // Начинаем отсчет затраченного времени на интервале (остановка на 76 строке)
+            for (int i = 0; i < 3000; i++) // Отрисовываем 3000 раз 8 фигур (всего 24000)
+            {
+                g.FillEllipse(Brushes.Black, x, 200, 10, 10);
+                g.FillRectangle(Brushes.Red, x, 210, 10, 10);
+                g.FillEllipse(Brushes.Black, x, 220, 10, 10);
+                g.FillRectangle(Brushes.Red, x, 230, 10, 10);
+                g.FillEllipse(Brushes.Black, x, 240, 10, 10);
+                g.FillRectangle(Brushes.Red, x, 250, 10, 10);
+                g.FillEllipse(Brushes.Black, x, 260, 10, 10);
+                g.FillRectangle(Brushes.Red, x, 270, 10, 10);
+                x = x + 0.09f; // Координату Х в конце каждой итерации инкрементируем на небольшое значение,
+                               // тем самым по оси Х происходит небольшое смещение
+            }
+            stopWatch.Stop(); // Останавливаем отсчет затраченного времени на интервале (запуск был на 62 строке)
+            g.Clear(SystemColors.ActiveCaption);
+
+            return stopWatch.ElapsedMilliseconds / 1000f; // Возвращаем значение затраченных секунд на выполнение теста 1
+        }
+
+        public double Test3() // Метод выполнения теста 3 (Запись и чтение файла). Возвращается время выполенения метода В СЕКУНДАХ+миллисекунды
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            string info = "ЭТА СТРОКА ЗАПИШЕТСЯ В ФАЙЛ TEST_3.txt 50000 РАЗ. ВРЕМЯ ЗАПИСИ ФАЙЛА БУДЕТ ЗАМЕРЯТЬСЯ";
+
+            stopWatch.Start(); // Начинаем отсчет затраченного времени на интервале (остановка на 91 строке)
+            for (int i = 0; i < 50000; i++)
+            {
+                File.AppendAllText("TEST_3.txt", info); // Метод создает или открывает файл,
+                                                        // записывает в него указанную строку по указанному пути и закрывается
+            }
+            stopWatch.Stop(); // Останавливаем отсчет затраченного времени на интервале (запуск был на 86 строке)
+
+            File.Delete("TEST_3.txt"); // Удаление только что заполненного текстового файла
+
+            return stopWatch.ElapsedMilliseconds / 1000f; // Возвращаем значение затраченных секунд на выполнение теста 1
+        }
+        #endregion
+
+        #region КЛИКИ НА КНОПКИ
+
+        private async void button1_Click(object sender, EventArgs e) // Событие нажатия на кнопку "НАЧАТЬ ТЕСТЫ"
+        {
+            string namePC = textBox1.Text;
+
+            if (textBox1.Text.Length > 0)
+            {
+                button1.Visible = false; // Делаем невидимой кнопку "НАЧАТЬ ТЕСТЫ"
+                button2.Visible = false; // Делаем невидимой кнопку "Удалить записи"
+                label3.Text = " "; // Очищаем лог для последующих записей в него
+
+                double test1Time = 0;
+                double test2Time = 0;
+                double test3Time = 0;
+
+                await Task.Run(() => // Асинхронные запуски методов и изменение свойств (асинхронность нужна для того, чтобы окно не зависло)
+                {
+                    label3.Text += "\n Начало сортировки \n......";
+                    test1Time = Test1(); // Выполнение теста 1
+                    label3.Text += "\n Сортировка завершена \n ...ТЕСТ 1 ЗАВЕРШЕН...";
+
+                    Thread.Sleep(2000); // Приостанавливаем поток на 2 секунды (смена теста)
+
+                    label3.Text += "\n Начало отрисовки графики... \n ......";
+                    test2Time = Test2(); // Тест 2
+                    label3.Text += "\n Отрисовка графики завершена \n ...ТЕСТ 2 ЗАВЕРШЕН...";
+
+                    Thread.Sleep(2000); // Приостанавливаем поток на 2 секунды (смена теста)
+
+                    label3.Text += "\n Начало записи в файл \n......";
+                    test3Time = Test3(); // Тест 3
+                    label3.Text += "\n Запись в файл завершена \n ...ТЕСТ 3 ЗАВЕРШЕН...";
+
+                });
+                label3.Text += "\n ТЕСТ 1 - " + test1Time + " секунд";
+                label3.Text += "\n ТЕСТ 2 - " + test2Time + " секунд";
+                label3.Text += "\n ТЕСТ 3 - " + test3Time + " секунд";
+
+                button1.Visible = true;
+                button2.Visible = true;
+
+                dataGridView1.Rows.Add(namePC, test1Time, test2Time, test3Time, Math.Pow(test1Time * test2Time * test3Time, 1.0 / 3));
+                dataGridView2.Rows.Add(
+                    namePC,
+                    test1Time / Convert.ToDouble(dataGridView1[1, 0].Value),
+                    test2Time / Convert.ToDouble(dataGridView1[2, 0].Value),
+                    test3Time / Convert.ToDouble(dataGridView1[3, 0].Value),
+                    Math.Pow((test1Time / Convert.ToDouble(dataGridView1[1, 0].Value)) *
+                            (test2Time / Convert.ToDouble(dataGridView1[2, 0].Value)) *
+                            (test3Time / Convert.ToDouble(dataGridView1[3, 0].Value)), 1.0 / 3)
+                    );
+
+
+            }
+            else MessageBox.Show("Введите имя тестируемого ПК", "", MessageBoxButtons.OK);
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Собитие при нажатии на кнопку "Удалить записи"
+        {
+            File.WriteAllText("sample.txt", string.Empty); // Перезаписываем файл (в итоге имеем пустой файл txt)
+            dataGridView1.Rows.Clear(); // Очищаем и сбрасываем таблицу
+            dataGridView1.Refresh();
+
+            dataGridView2.Rows.Clear();
+            dataGridView2.Refresh();
+        }
+        #endregion
+
+        #region ЗАГРУЗКА ТАБЛИЦЫ ПРИ ОТКРЫТИТИ И СОХРАНЕНИЕ ПРИ ЗАКРЫТИТИ
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) // Событие закрытия окна, тут выполняется запись предыдущих тестов
+        {
+
+            StreamWriter file = new StreamWriter("sample.txt");
+            string sLine = "";
+
+            // Этот цикл for повторяется через каждую строку в таблице
+            for (int r = 0; r <= dataGridView1.Rows.Count - 1; r++)
+            {
+                //Это для цикла, проходящего через каждый столбец, и номер строки
+                //передается из цикла for выше.
+                for (int c = 0; c <= dataGridView1.Columns.Count - 1; c++)
+                {
+                    sLine = sLine + dataGridView1.Rows[r].Cells[c].Value;
+                    if (c != dataGridView1.Columns.Count - 1)
+                    {
+                        //Символ "|" добавляется в качестве разделителя значений в таблице
+                        sLine = sLine + "|";
+                    }
+                }
+                //Текст записывается в текстовый файл построчно
+                file.WriteLine(sLine);
+                sLine = "";
+            }
+
+            file.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) // Событие загрузки окна, тут выполняется чтение записей из текстового файла в нашу таблицу в программе
+        {
+            string[] lines = File.ReadAllLines("sample.txt");
+            string[] values;
+
+            // Этот цикл for повторяется через каждую строку в таблице
+            for (int i = 0; i < lines.Length; i++)
+            {
+                values = lines[i].ToString().Split('|'); // Разделяем значения через поиск символа "|" и присваиваем значение в i элемент массива
+                string[] row = new string[values.Length];
+
+                for (int j = 0; j < values.Length; j++)
+                {
+                    row[j] = values[j].Trim();
+                }
+                dataGridView1.Rows.Add(row);
+            }
+
+            for (int i = 0; i < dataGridView1.RowCount; i++) // Запись во вторую таблицу осуществляется через расчеты. Т.к нужно всего лишь посчитать коэффициенты 
+            {
+                dataGridView2.Rows.Add(
+                    dataGridView1[0, i].Value,
+                    Convert.ToDouble(dataGridView1[1, i].Value) / Convert.ToDouble(dataGridView1[1, 0].Value),
+                    Convert.ToDouble(dataGridView1[2, i].Value) / Convert.ToDouble(dataGridView1[2, 0].Value),
+                    Convert.ToDouble(dataGridView1[3, i].Value) / Convert.ToDouble(dataGridView1[3, 0].Value),
+                    Convert.ToDouble(dataGridView1[4, i].Value) / Convert.ToDouble(dataGridView1[4, 0].Value)
+                    );
+            }
+        }
+        #endregion
+
+    }
+}
